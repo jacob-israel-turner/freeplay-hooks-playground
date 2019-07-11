16f4448418fb61da02dd82a9e75593c6d5d8daee
Jacob Turner <j.israel.turner@gmail.com>
2019-07-11 16:33:36 -0600

    Transition to hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
-  );
+  )
 }
+
+function Counter () {
+  const [count, updateCount] = useState(0)
+  const [time, updateTime] = useState(0)
+
+  useEffect(() => {
+    document.title = `Clicked ${count} times`
+  }, [count])
+
+  useEffect(() => {
+    const t = setInterval(() => {
+      updateTime(t => t + 1)
+    }, 1000)
+    return () => clearInterval(t)
+  }, [])
+
+  return (
+    <div>
+      <p>You have been here {time} seconds!</p>
+      <p>You clicked the counter {count} times!</p>
+      <button onClick={() => updateCount(count + 1)}>Click!</ button>
+    </div>
+  )
+}
+
+// class Counter extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       count: 0
+//     }
+//     this.handleClick = this.handleClick.bind(this)
+//     this.updateDocTitle = this.updateDocTitle.bind(this)
+//   }
+//
+//   updateDocTitle () {
+//     document.title = `Clicked ${this.state.count} times`
+//   }
+//
+//   componentDidMount() {
+//     this.updateDocTitle()
+//   }
+//
+//   componentDidUpdate() {
+//     this.updateDocTitle()
+//   }
+//
+//   handleClick () {
+//     this.setState({ count: this.state.count + 1 })
+//   }
+//
+//   render () {
+//     return (
+//       <div>
+//         <p>You clicked the counter {this.state.count} times!</p>
+//         <button onClick={this.handleClick}>Click!</ button>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
